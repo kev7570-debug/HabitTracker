88b046f32ccaaf06422334fec95915d1514c7e7d
elena <kev7570@gmail.com>
2026-08-22 16:56:33 +0300

    Added: serializers, validators, permissions, pagination, public endpoint

--- habits/urls.py
+++ habits/urls.py
@@ -1,3 +1,24 @@
-from django.urls import path
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from .views import HabitViewSet, public_habits_list
+#
+# router = DefaultRouter()
+# router.register(r'habits', HabitViewSet, basename='habit')
+#
+#
+# urlpatterns = [
+#     path('public/', public_habits_list, name='public-habits'),
+#     path('', include(router.urls)),
+# ]
 
-urlpatterns = []
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import HabitViewSet
+
+router = DefaultRouter()
+router.register(r'habits', HabitViewSet, basename='habit')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
