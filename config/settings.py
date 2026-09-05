9a273ba4014b98f5ef3d2fb84b6a8054cc23098f
elena <kev7570@gmail.com>
2026-09-05 15:39:58 +0300

    Add SECRET_KEY fallback for CI

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-for-ci')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
